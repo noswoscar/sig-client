552bd4dbc919f98f28d77a67081e401f7d6fbd92
noswoscar <32093588+noswoscar@users.noreply.github.com>
2023-09-12 14:07:01 +0200

    deleted .js

--- src/lib/mapbox.js
+++
@@ -1,7 +0,0 @@
-// import mapbox from 'mapbox-gl';
-// // https://docs.mapbox.com/help/glossary/access-token/
-// mapbox.accessToken = "ddadazdazza";
-
-// const keyApp = {};
-
-// export { mapbox, keyApp };
